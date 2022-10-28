19ca0768a1853e63f68f67c8224fd77ebf02d192
kumilachew-g <kumilachew1212@gmail.com>
2022-10-28 11:08:42 +0300

    Fixed based on review

--- my_list.rb
+++ my_list.rb
@@ -2,25 +2,20 @@ require_relative 'my_enumerable'
 
 class MyList
   include MyEnumerable
+
   def initialize(*list)
     @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
   end
 end
 
 list = MyList.new(1, 2, 3, 4)
 
-puts 'Test #all?'
-puts(list.all? { |n| n < 5 })
-puts '|n| n < 5 Should return true'
-puts(list.all? { |n| n > 5 })
-puts '|n| n > 5 Should return false'
-
-puts 'Test #any?'
-puts '|e| e == 2 Should return true'
-puts(list.any? { |e| e == 2 })
-puts '|e| |e| e == 5 Should return false'
-puts(list.any? { |e| e == 5 })
-
-puts 'Test #filter'
-puts '|e| e.even? Should return [2,4]'
-puts(list.filter?(&:even?))
+puts(list.all? { |e| e <= 5 }) # true
+puts(list.all? { |e| e >= 5 }) # false
+puts(list.any? { |e| e >= 5 }) # true
+puts(list.any? { |e| e > 5 }) # false
+puts(list.filter? { |e| e.even? }) # [2, 4]
